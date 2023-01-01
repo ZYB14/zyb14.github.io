5fcb4060d8554d17372b4f2476e7105d32b477ff
ZYB14 <107764315+ZYB14@users.noreply.github.com>
2023-01-01 20:42:33 +0800

    Add files via upload

---
+++ bingpic.php
@@ -0,0 +1,1 @@
+<?php $str=file_get_contents('https://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');if (preg_match("/(.+?)/ies", $str, $matches)) { $imgurl='https://cn.bing.com'.$matches[1];}if ($imgurl) { header('Content-Type: image/JPEG'); @ob_end_clean(); @readfile($imgurl); @flush(); @ob_flush(); exit();} else { exit('error');}?>
